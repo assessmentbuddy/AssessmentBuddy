8298eff2ce65d152b13a3d9056bad56428a74adc
David Hovemeyer <david.hovemeyer@gmail.com>
2015-06-02 12:46:31 -0400

    test adding indicators to outcomes

--- grails-app/services/org/assessmentbuddy/model/InitialDataService.groovy
+++ grails-app/services/org/assessmentbuddy/model/InitialDataService.groovy
@@ -25,6 +25,38 @@ class InitialDataService {
             description: "An ability to analyze a problem, and identify and define the computing requirements appropriate to its solution"
         )
         
+        def indA1 = new Indicator(
+            shortName: "a.1",
+            description: "Student can correctly interpret a computational problem and define its parameters"
+        )
+        def indA2 = new Indicator(
+            shortName: "a.2",
+            description: "Student can analyze a computation problem in order to choose mathematical and algorithmic principles that can be applied to solve the problem"
+        )
+        def indA3 = new Indicator(
+            shortName: "a.3",
+            description: "Student can define a solution to a computational problem"
+        )
+        outcomeA.addToIndicators(indA1)
+        outcomeA.addToIndicators(indA2)
+        outcomeA.addToIndicators(indA3)
+        
+        def indB1 = new Indicator(
+            shortName: "b.1",
+            description: "Student can effectively collect and document system requirements"
+        )
+        def indB2 = new Indicator(
+            shortName: "b.2",
+            description: "Student can effectively analyze and model a problem domain"
+        )
+        def indB3 = new Indicator(
+            shortName: "b.3",
+            description: "Student can identify and evaluate appropriate technologies to be used in a system"
+        )
+        outcomeB.addToIndicators(indB1)
+        outcomeB.addToIndicators(indB2)
+        outcomeB.addToIndicators(indB3)
+        
         p.addToOutcomes(outcomeA)
         p.addToOutcomes(outcomeB)
         
--- test/integration/org/assessmentbuddy/model/ProgramIntegrationSpec.groovy
+++ test/integration/org/assessmentbuddy/model/ProgramIntegrationSpec.groovy
@@ -21,6 +21,47 @@ class ProgramIntegrationSpec extends IntegrationSpec {
         
         then: "check properties and number of outcomes"
         p.outcomes.size() == 2
+    }
+    
+    void "find program and check that correct outcomes are found"() {
+        given: "an existing program"
         
+        when: "loading an existing program"
+        def p = Program.findByName("Computer Science")
+        
+        then: "its outcomes should be the expected ones"
+        def (outcomeA, outcomeB) = p.outcomes.sort { it.shortName }
+        
+        outcomeA.shortName == "a"
+        outcomeA.description.startsWith("An ability to apply knowledge of computing and mathematics")
+        
+        outcomeB.shortName == "b"
+        outcomeB.description.startsWith("An ability to analyze a problem, and identify")
+    }
+    
+    void "find a program and check that correct indicators are found"() {
+        given: "an existing program"
+        
+        when: "loading an existing program"
+        def p = Program.findByName("Computer Science")
+        
+        then: "its indicators should be the expected ones"
+        def (outcomeA, outcomeB) = p.outcomes.sort { it.shortName }
+
+        def (indA1, indA2, indA3) = outcomeA.indicators.sort { it.shortName }
+        indA1.shortName == "a.1"
+        indA1.description.startsWith("Student can correctly interpret a computational problem")
+        indA2.shortName == "a.2"
+        indA2.description.startsWith("Student can analyze a computation problem in order")
+        indA3.shortName == "a.3"
+        indA3.description.startsWith("Student can define a solution to a computational")
+        
+        def (indB1, indB2, indB3) = outcomeB.indicators.sort { it.shortName }
+        indB1.shortName == "b.1"
+        indB1.description.startsWith("Student can effectively collect and document")
+        indB2.shortName == "b.2"
+        indB2.description.startsWith("Student can effectively analyze and model a")
+        indB3.shortName == "b.3"
+        indB3.description.startsWith("Student can identify and evaluate appropriate technologies")
     }
 }
