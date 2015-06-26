56edf5bbdfae3eb41d6d2efa106552ec133f398b
David Hovemeyer <david.hovemeyer@gmail.com>
2015-06-26 13:51:05 -0400

    populate achievement levels when a rubric is selected

--- grails-app/assets/javascripts/assessmentbuddy.js
+++ grails-app/assets/javascripts/assessmentbuddy.js
@@ -8,6 +8,20 @@ window.ab = (function($) {
     }
     
     // For creating/editing a target.
+    // Update the achievement level selections based on the
+    // specified array of AchievementLevel objects.
+    function setAchievementLevels(achievementLevels) {
+        var select = $("#achievementLevelId");
+        // Remove all options but first
+        select.find("option:gt(0)").remove();
+        // Add options for achievement levels
+        achievementLevels.forEach(function(val, index, arr) {
+            console.log("appending id=" + val.id + ",name=" + val.name);
+            select.append($("<option></option>").attr("value", val.id).text(val.name));
+        });
+    }
+    
+    // For creating/editing a target.
     // When a rubric is selected, allow the user to select
     // the achievement levels for that rubric.
     function getAchievementLevelsForRubric(ajaxUrl) {
@@ -18,6 +32,7 @@ window.ab = (function($) {
             dataType: 'json',
             success: function(data, status, jqXHR) {
                 console.log("Received " + data.length + " achievement level(s)");
+                setAchievementLevels(data);
             },
             error: function(jqXHR, textStatus, errorThrown) {
                 console.log("Error receiving achievement levels: " + errorThrown);
@@ -28,6 +43,7 @@ window.ab = (function($) {
     // Return exported functions
     return {
         init: init,
-        getAchievementLevelsForRubric: getAchievementLevelsForRubric
+        getAchievementLevelsForRubric: getAchievementLevelsForRubric,
+        setAchievementLevels: setAchievementLevels
     };
 })(jQuery);
